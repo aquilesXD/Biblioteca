be73d1ba262f8cd5f47dca29b454828a90c62564
Aquiles Gonzalez <aquilesbarboza1989@gmail.com>
2024-02-10 16:32:57 -0400

    autentificacion de usuario con JWT

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('jwt', ['except' => ['login', 'register']]);
+    }
+
+    public function register(){
+        $user = new User(request()->all());
+        $user->password = bcrypt($user->password);
+        $user->save();
+        return response()->json(["data"=>$user], 200);   
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+}
